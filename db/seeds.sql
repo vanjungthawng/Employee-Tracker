feaf88008ad3ffc048bd5457051794e354384f84
Van Cung Thawnge <vanjungthawng@gmail.com>
2021-11-15 16:59:16 +1100

    create seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('Bryan', 'MC', 1, null),
+('Alice', 'Spring', 3, 1),
+('Tom', 'Cruise', 5, 2),
+('John', 'Doe', 4, null),
+('Sam', 'Din', 6, null),
+('Shawn', 'Mendas', 8, null),
+('James', 'Lau', 15, null);
+
+-- add departments
+INSERT INTO department (department_name)
+VALUES
+('Reception'),
+('Human Resources'),
+('Finance'),
+('Software Engineer'),
+('Sales'),
+('Marketing');
+
+-- roles
+INSERT INTO role (title, salary, department_id)
+VALUES 
+('Salesman', 80000, 2),
+('Finance', 90000, 4),
+('Senior Engineer', 120000, 7),
+('Receptionist', 70000, 13),
+('Digital Marketing Manager', 110000, 15),
+('Human Resource Officer', 130000, 12);
